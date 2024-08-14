0b5661aaa19e74de9a4684a430e88f55da6253bf
user <a.habboush04@gmail.com>
2024-08-14 22:09:57 +0300

    made basic layout and added units in html

---
+++ app.js
@@ -0,0 +1,11 @@
+const all_content = document.querySelectorAll("content");
+
+const content_length = document.getElementById("length");
+const content_temp = document.getElementById("temperature");
+const content_area = document.getElementById("area");
+const content_volume = document.getElementById("volume");
+const content_weight = document.getElementById("weight");
+const content_time = document.getElementById("time");
+
+
+content_time.classList.add("active");
