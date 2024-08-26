1cd71a69802da2b6f98fb739bf4b8383590c0cd8
Abdulrahman Habboush <a.habboush04@gmail.com>
2024-08-26 17:19:02 +0300

    3/6 conversions done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const all_units = document.querySelectorAll(".units");
-const all_tabs = document.querySelectorAll(".tabs button")
+const all_tabs = document.querySelectorAll(".tab_btn")
 
 
 // default
@@ -11,7 +11,7 @@ all_tabs.forEach((tab, index) => {
 
     // on click
     tab.addEventListener('click', ()=>{
-
+      
         // only one tab active at a time
         all_tabs.forEach(tab=>{
             tab.classList.remove('active');
@@ -33,51 +33,56 @@ all_tabs.forEach((tab, index) => {
 
 let convertButton = document.getElementById("convertButton");
 
+
+
+
 convertButton.addEventListener('click', ()=>{
 
-
-let inputLength = document.getElementById("from_length").value;
-let outputLength = document.getElementById("to_length");
-let fromUnitLength = document.getElementById("from_units_length").value;
-let toUnitLength = document.getElementById("to_units_length").value;
-let lengthInMeters;
-let answerLength;
+  let currentTab = document.querySelector(".tab_btn.active").id;
+  let currentFromUnit = document.querySelector(".units.active select").value;
+  let currentToUnit = document.querySelector(".units.active select:last-of-type").value;
+  let inputText = parseFloat(document.getElementById("inputTextbox").value);
+  let outputTextbox = document.getElementById("outputTextbox");
 
 
-switch(fromUnitLength) {
+if (currentTab === "length_button"){
+  let lengthInMeters;
+  let answerLength;
+  
+  switch(currentFromUnit) {
     case "km":
-        lengthInMeters = inputLength * 1000;  
+        lengthInMeters = inputText * 1000;  
       break;
 
     case "m":
-      lengthInMeters = inputLength;  
+      lengthInMeters = inputText;  
       break;
 
     case "cm":
-    lengthInMeters = inputLength / 100;
+    lengthInMeters = inputText / 100;
     break;
 
     case "mi":
-    lengthInMeters = inputLength * 1609.343;
+    lengthInMeters = inputText * 1609.343;
     break;
 
     case "ya":
-    lengthInMeters = inputLength / 1.09368;
+    lengthInMeters = inputText / 1.09368;
     break;
 
     case "in":
-    lengthInMeters = inputLength * 0.0254;
+    lengthInMeters = inputText * 0.0254;
     break;
 
     case "ft":
-    lengthInMeters = inputLength / 3.281;
+    lengthInMeters = inputText / 3.281;
     break;
 
     default:
         lengthInMeters = 0;
   } 
 
-  switch(toUnitLength){
+  switch(currentToUnit){
     
     case "km":
         answerLength = Math.round((lengthInMeters / 1000) * 100000) / 100000;  
@@ -96,7 +101,7 @@ switch(fromUnitLength) {
     break;
 
     case "ya":
-    answerLength =  Math.round((lengthInMeters * 1.094) * 100000) / 100000;
+    answerLength =  Math.round((lengthInMeters * 1.093614) * 1000) / 1000;
     break;
 
     case "in":
@@ -109,8 +114,102 @@ switch(fromUnitLength) {
 
   }
 
-outputLength.value = answerLength;
+outputTextbox.value = answerLength;
+}
 
+if (currentTab === "temp_button"){
+let tempInK = 0;
+let answerTemp = 0;
+
+  switch(currentFromUnit){
+
+  case "c":
+    tempInK = (inputText + 273.15);
+    break;
+
+  case "f":
+    tempInK = ((inputText - 32) * (5/9)) + 273.15;
+    break;
+
+  case "k":
+    tempInK = inputText;
+    break;
+  }
+
+  switch(currentToUnit){
+    case "c":
+    answerTemp = Math.round((tempInK - 273.15) * 1000) / 1000;
+    break;
+
+  case "f":
+    answerTemp = Math.round(((tempInK - 273.15) * (9/5) + 32) * 1000) / 1000;
+    break;
+
+  case "k":
+    answerTemp = Math.round((tempInK) * 1000) / 1000;
+    break;
+  }
+
+  outputTextbox.value = answerTemp;
+}
+
+if (currentTab === "area_button"){
+  let areaInSqm = 0;
+  let answerArea = 0;
+  
+    switch(currentFromUnit){
+  
+    case "sqm":
+      areaInSqm = inputText;
+      break;
+  
+    case "sqkm":
+      areaInSqm = inputText * 1000000;
+      break;
+    
+    case "acre":
+      areaInSqm = inputText * 4046.86;
+      break;
+
+    case "sqft":
+      areaInSqm = inputText / 10.764;
+      break;
+    case "sqy":
+      areaInSqm = inputText / 1.196;
+      break;
+    case "sqmi":
+      areaInSqm = inputText * 2589988.11;
+      break;
+    
+    }
+  
+    switch(currentToUnit){
+      case "sqm":
+        answerArea = Math.round((areaInSqm) * 1000) / 1000;
+        break;
+    
+      case "sqkm":
+        answerArea = Math.round((areaInSqm / 1000000) * 10000000) / 10000000;
+        break;
+      
+      case "acre":
+        answerArea = Math.round((areaInSqm / 4046.86) * 1000000) / 1000000;
+        break;
+  
+      case "sqft":
+        answerArea = Math.round((areaInSqm * 10.764) * 100) / 100;
+        break;
+      case "sqy":
+        answerArea = Math.round((areaInSqm * 1.196) * 1000) / 1000;
+        break;
+      case "sqmi":
+        answerArea = Math.round((areaInSqm / 2589988.11) * 1000000000) / 1000000000;
+        break;
+      
+    }
+  
+    outputTextbox.value = answerArea;
+  }
 
 })
 
