9f1c27252cdad1e09fb9651bc522c4cdcc816e1b
Abdulrahman Habboush <a.habboush04@gmail.com>
2024-08-27 02:02:31 +0300

    made conversions work and textboxes clear between tabs

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const all_units = document.querySelectorAll(".units");
 const all_tabs = document.querySelectorAll(".tab_btn")
 
 
-// default
+// default active
 all_units[0].classList.add('active');
 all_tabs[0].classList.add('active');
 
 
 all_tabs.forEach((tab, index) => {
 
-    // on click
+    // on tab button click
     tab.addEventListener('click', ()=>{
       
         // only one tab active at a time
@@ -19,8 +19,10 @@ all_tabs.forEach((tab, index) => {
 
         tab.classList.add('active');
 
-        // only one tab content is active
+        // only one tab content is active and clear textboxes
         all_units.forEach(content=>{
+          document.getElementById("inputTextbox").value = " ";
+          document.getElementById("outputTextbox").value = " ";
             content.classList.remove('active');
         });
     
@@ -31,13 +33,15 @@ all_tabs.forEach((tab, index) => {
 
 
 
+
+//todo: make a button to swap units around
+
+
 let convertButton = document.getElementById("convertButton");
-
-
-
 
 convertButton.addEventListener('click', ()=>{
 
+  // updates with current information
   let currentTab = document.querySelector(".tab_btn.active").id;
   let currentFromUnit = document.querySelector(".units.active select").value;
   let currentToUnit = document.querySelector(".units.active select:last-of-type").value;
@@ -45,6 +49,7 @@ convertButton.addEventListener('click', ()=>{
   let outputTextbox = document.getElementById("outputTextbox");
 
 
+  // conditional conversion formulas
 if (currentTab === "length_button"){
   let lengthInMeters;
   let answerLength;
@@ -210,6 +215,183 @@ if (currentTab === "area_button"){
   
     outputTextbox.value = answerArea;
   }
+
+if (currentTab === "volume_button"){
+  let volumeInLitre = 0;
+  let answerVolume = 0;
+  
+    switch(currentFromUnit){
+  
+    case "m3":
+      volumeInLitre = inputText * 1000;
+      break;
+  
+    case "ml":
+      volumeInLitre = inputText / 1000;
+      break;
+    
+    case "l":
+      volumeInLitre = inputText;
+      break;
+
+    case "floz":
+      volumeInLitre = inputText / 33.814;
+      break;
+    case "gn":
+      volumeInLitre = inputText * 3.785;
+      break;
+  
+    
+    }
+  
+    switch(currentToUnit){
+      case "m3":
+      answerVolume = Math.round((volumeInLitre / 1000) * 10000000) / 10000000;
+      break;
+  
+    case "ml":
+      answerVolume = Math.round((volumeInLitre * 1000) * 100) / 100;
+      break;
+    
+    case "l":
+      answerVolume = Math.round((volumeInLitre) * 10000) / 10000;
+      break;
+
+    case "floz":
+      answerVolume = Math.round((volumeInLitre * 33.814022) * 100) / 100;
+      break;
+    case "gn":
+      answerVolume = Math.round((volumeInLitre / 3.785) * 10000000) / 10000000;
+      break;
+      
+    }
+  
+    outputTextbox.value = answerVolume;
+  }
+
+if (currentTab === "weight_button"){
+    let weightInKg = 0;
+    let answerWeight = 0;
+    
+      switch(currentFromUnit){
+    
+      case "g":
+        weightInKg = inputText / 1000;
+        break;
+    
+      case "kg":
+        weightInKg = inputText;
+        break;
+      
+      case "lb":
+        weightInKg = inputText / 2.204625;
+        break;
+  
+      case "oz":
+        weightInKg = inputText / 35.274;
+        break;
+      case "t":
+        weightInKg = inputText * 1000;
+        break;
+    
+      
+      }
+    
+      switch(currentToUnit){
+        case "g":
+        answerWeight = Math.round((weightInKg * 1000) * 100) / 100;
+        break;
+    
+      case "kg":
+        answerWeight = Math.round((weightInKg) * 10000) / 10000;
+        break;
+      
+      case "lb":
+        answerWeight = Math.round((weightInKg * 2.204625) * 1000) / 1000;
+        break;
+  
+      case "oz":
+        answerWeight = Math.round((weightInKg * 35.274) * 100) / 100;
+        break;
+      case "t":
+        answerWeight = Math.round((weightInKg / 1000) * 1000000) / 1000000;
+        break;
+        
+      }
+    
+      outputTextbox.value = answerWeight;
+    }
+
+if (currentTab === "time_button"){
+      let timeInHours = 0;
+      let answerTime = 0;
+      
+        switch(currentFromUnit){
+      
+        case "s":
+          timeInHours = inputText / 3600;
+          break;
+      
+        case "min":
+          timeInHours = inputText / 60;
+          break;
+        
+        case "hour":
+          timeInHours = inputText;
+          break;
+    
+        case "day":
+          timeInHours = inputText * 24;
+          break;
+        case "week":
+          timeInHours = inputText * 168;
+          break;
+          case "mon":
+          timeInHours = inputText * 730;
+          break;
+          case "year":
+          timeInHours = inputText * 8760;
+          break;
+          case "decade":
+          timeInHours = inputText * 87600;
+          break;
+
+        }
+      
+        switch(currentToUnit){
+
+            case "s":
+            answerTime = Math.round((timeInHours * 3600) * 10) / 10;
+            break;
+        
+          case "min":
+            answerTime = Math.round((timeInHours * 60) * 10000) / 10000;
+            break;
+          
+          case "hour":
+            answerTime = Math.round((timeInHours) * 10000) / 10000;
+            break;
+      
+          case "day":
+            answerTime = Math.round((timeInHours / 24) * 10000) / 10000;
+            break;
+          case "week":
+            answerTime = Math.round((timeInHours / 168) * 1000000) / 1000000;
+            break;
+            case "mon":
+            answerTime = Math.round((timeInHours / 730) * 10000000) / 10000000;
+            break;
+            case "year":
+            answerTime = Math.round((timeInHours / 8760) * 100000000) / 100000000;
+            break;
+            case "decade":
+            answerTime = Math.round((timeInHours / 87600) * 1000000000) / 1000000000;
+            break;
+          
+        }
+      
+        outputTextbox.value = answerTime;
+      }
 
 })
 
